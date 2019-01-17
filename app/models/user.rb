4803ed20440adc0e0f8ba82566c790164c364896
Ruslan Knyazev <agrry@yandex.ru>
2019-01-17 14:41:01 +0500

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, uniqueness: true
 end

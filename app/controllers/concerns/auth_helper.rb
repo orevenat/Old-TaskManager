464d1837c27ee279ef8d4baecdd701af2de5e072
orevenat <agrry@yandex.ru>
2019-01-28 23:50:41 +0500

    files update

--- app/controllers/concerns/auth_helper.rb
+++ app/controllers/concerns/auth_helper.rb
@@ -12,14 +12,12 @@ module Concerns
       current_user.present?
     end
 
-
     def authenticate_user!
       redirect_to new_session_path unless signed_in?
     end
 
     def current_user
       return if session[:user_id].blank?
-
       @_current_user ||= User.find_by(id: session[:user_id])
     end
   end

a9c1dbb8be778deda1b76bdc08faeca85aff2113
orevenat <agrry@yandex.ru>
2019-02-01 13:45:58 +0500

    partial && sequence

--- test/factories/sequences.rb
+++ test/factories/sequences.rb
@@ -5,4 +5,16 @@ FactoryBot.define do
   sequence :string do |n|
     "string#{n}"
   end
+  sequence :first_name do |n|
+    "first_name_#{n}"
+  end
+  sequence :last_name do |n|
+    "last_name_#{n}"
+  end
+  sequence :password do |n|
+    "pswrd#{n}"
+  end
+  sequence :description do |n|
+    "description #{n}"
+  end
 end

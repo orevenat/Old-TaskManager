bb896fcbce4f6ec10c7cf42bea605b8f3f878020
orevenat <agrry@yandex.ru>
2019-03-01 17:47:58 +0500

    Add axios and Fetch.js

---
+++ app/javascript/packs/components/Fetch.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export function authenticityToken () {
+  const token = document.quertySelector('meta[name="csrf-token"]')
+  return token ? token.content : null
+}
+
+function headers () {
+  return {
+    Accept: '*/*',
+    'content-Type': 'application/json',
+    'X-CSRF-Token': authenticityToken(),
+    'X-Requested-With': 'XMLHttpRequest'
+  }
+}
+
+export function fetch (method, url, body) {
+  const options = {
+    method,
+    headers: headers(),
+    data: body,
+    url
+  }
+
+  return axios(options)
+}
